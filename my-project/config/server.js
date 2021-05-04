307b58f153d55e729226cc97b2c38341288f2be8
homiramine <aminehr4@gmail.com>
2021-05-04 11:34:34 +0000

    up

--- my-project/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'f8784829ed56c480230fd1d722508c79'),
-    },
-  },
-});
--- my-project/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '1e2a5527-c05e-4b4e-aaba-c555013fe1a5'
-};
